3c5c85c0aa1c6a42f801a449e41852c02bdbdca8
daheung <saasaa001@naver.com>
2026-08-21 06:48:17 +0900

    - Add test network code.

--- Testbed/Source/Game.c
+++ Testbed/Source/Game.c
@@ -2,8 +2,25 @@
 
 #include <Core/Logger.h>
 
+#include "Runtime/Sockets/Sockets.h"
+#include "Runtime/Sockets/SocketSubsystem.h"
+#include "Core/Containers/Array.h"
+
+static FSocketSubsystemState GSocketSubsystem;
+static FSocket  GClientSocket;
+
 bool8 GameInitialize(struct FGame* GameInstance)
 {
+    InitializeSocketSubsystem(&GSocketSubsystem);
+
+    GClientSocket = CreateSocket(SOCKET_TYPE_STREAMING);
+
+    FInternetAddress BindAddress = {
+        .Ip = "192.168.3.132",
+        .Port = 10000
+    };
+    const bool8 bConnectSucceed = Connect(&GClientSocket, BindAddress);
+
     LUMORA_DEBUG("GameInitialize() called");
     return TRUE;
 }
@@ -20,4 +37,9 @@ bool8 GameRender(struct FGame *GameInstance, float32 DeltaTime)
 
 void GameOnResize(struct FGame *GameInstance, uint32 Width, uint32 Height)
 {
+}
+
+bool8 GameRelease(struct FGame* GameInstance)
+{
+    return TRUE;
 }
